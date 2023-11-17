567218bc95d7125f01197949fbe55c3455cf879f
星图辛某人 <117000608+11xy11@users.noreply.github.com>
2023-11-17 16:36:28 +0800

    Add files via upload

---
+++ Chapter  1/program1-18.cpp
@@ -0,0 +1,32 @@
+#include"iostream"
+#include"currency1.h"
+using namespace std;
+
+int main()
+{
+	currency g,h(PLUS,3,50),i,j;
+	
+	g.setValue(MINUS,2,25);
+	i.setValue(-6.45);
+	j=h.add(g);
+	
+	h.output();
+	cout<<"+";
+	g.output();
+	cout<<"=";
+	j.output();
+	
+	j=i.add(g).add(h);//j=g+h
+	j=i.increment(g).add(h);//j=g+hÇÒi=i+g
+	 
+	cout<<"Attemping to initialize with cents=152"<<endl;
+	try{
+		i.setValue(PLUS,3,152);
+	}
+	catch(illegaiParameter e)
+	{
+		cout<<"Caught thrown exception"<<endl;
+		e.outputMessage(); 
+	}
+	return 0;
+ } 
